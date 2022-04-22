3cbb49c555ae93e6839eb12b85fe5e48f523c64a
Mark Chuang <markchuangth@gmail.com>
2022-04-21 20:44:28 -0400

    set up question 1

---
+++ tests/w1/q1.js
@@ -0,0 +1,68 @@
+/* Question 1
+ *
+ *  Implement the functions defined below
+ *
+ */
+
+
+
+/* ===========================================================================
+
+// Question 1.1 
+
+  * MIN - the lowest value in a list
+  *
+  * For example:
+  *
+  *    min([6,2,3,4,9,6,1,0,5])
+  *
+  * Returns:
+  *
+  *    0
+  */
+
+
+  const min = function(arr) {
+
+  };
+
+
+/* ===========================================================================
+
+  // Question 1.2
+
+  * MAX - the highest value in a list
+  *
+  * For example:
+  *
+  *    max([6,2,3,4,9,6,1,0,5])
+  *
+  * Returns:
+  *
+  *    9
+  */
+  const max = function(arr) {
+
+  };
+
+/* ===========================================================================
+
+Question 1.3 
+
+  * RANGE - the difference between the highest and lowest values in a list
+  *
+  * For example:
+  *
+  *    range([6,2,3,4,9,6,1,0,5])
+  *
+  * Returns:
+  *
+  *    9
+  */
+  const range = function(arr) {
+    
+    
+  };
+
+
+
