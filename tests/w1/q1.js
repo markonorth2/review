c864c99558691d212a57877cab065ba1ace6bfea
Mark Chuang <markchuangth@gmail.com>
2022-04-21 21:02:57 -0400

    question 1.1 works if no zero in array

--- tests/w1/q1.js
+++ tests/w1/q1.js
@@ -23,10 +23,25 @@
 
 
   const min = function(arr) {
-
+    // We need a variable to store the minimum value as we loop through the array. We will compare the min variable to the current value, the smallest one becomes the min   
+    let min = null
+    for (const value of arr) {
+      
+      if (min){
+        if (value < min){
+          min = value
+          console.log("success", min)
+        }
+      }
+      else {
+        min = value
+        console.log("else", min)
+      }
+    }
+    return min 
   };
 
-
+console.log("1.1 Answer:", min([6,2,3,4,9,6,1,0,5]))
 /* ===========================================================================
 
   // Question 1.2
